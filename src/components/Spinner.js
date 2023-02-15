bcff16d11f7c959aa7a008136496d71631ef4d64
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-02-15 09:35:58 -0300

    first commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  '../assets/css/Spinner.css';
+
+const Spinner = () => {
+
+  return (
+    
+<div className="lds-ripple">
+  <div></div>
+  <div></div>
+</div>
+
+  );
+
+}
+
+export default Spinner;
---
+++ src/components/WeatherPanel.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-throw-literal */
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+import React, { useState} from 'react';
+import Form from './Formulario';
+import Card from './Card';
+
+const WeatherPanel = () => {
+
+    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?appid=c93dc315927127214bf93e7cfb33ecfc&lang=es";
+    let cityUrl = "&q=";
+
+    let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=c93dc315927127214bf93e7cfb33ecfc&lang=es";
+
+    const [weather, setWeather] = useState();
+    const [forecast, setForecast] = useState();
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [location, setLocation] = useState("");
+    
+    const getLocation = async(loc) => {
+        setLoading(true);
+        setLocation(loc);
+
+        //weather
+        urlWeather = urlWeather + cityUrl + loc;
+
+        await fetch(urlWeather).then((response) =>{
+            if(!response.ok) throw {response}
+            return response.json();
+        }).then((weatherData) =>{
+            console.log(weatherData);
+            setWeather(weatherData);
+        }).catch(error =>{
+            console.log (error);
+            setLoading(false);
+            setShow(false);
+        });
+
+        // Forecast
+        urlForecast = urlForecast + cityUrl + loc; 
+       
+        await fetch (urlForecast).then((response) => {
+            if(!response.ok) throw {response}
+            return response.json();
+        }).then((forecastData) =>{
+            console.log(forecastData);
+            setForecast(forecastData);
+            
+            setLoading(false);
+            setShow(true);
+
+
+        }).catch(error =>{
+            console.log(error);
+            setLoading(false);
+            setShow(false);
+        });
+    }
+
+    return(
+       <React.Fragment>
+           <Form
+           newLocation = {getLocation}
+            />
+
+            <Card
+            showData = {show}
+            loadingData = {loading}
+            weather = {weather}
+            forecast = {forecast}
+            location ={location}
+            />
+        
+       </React.Fragment>
+    )
+
+}
+
+export default WeatherPanel;
